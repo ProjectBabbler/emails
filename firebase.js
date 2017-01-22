b29e540630011fc01c2377e1ee96d08e44238bea
Greg Schechter <diabolomaster@gmail.com>
2017-01-21 19:27:23 -0800

    Fix firebase key

--- firebase.js
+++ firebase.js
@@ -4,7 +4,7 @@ var config = {
     credential: admin.credential.cert({
         projectId: "birder-emails",
         clientEmail: "firebase-adminsdk-8dshl@birder-emails.iam.gserviceaccount.com",
-        privateKey: process.env.FIREBASE_KEY,
+        privateKey: process.env.FIREBASE_KEY.replace(/\\n/g, '\n'),
     }),
     databaseURL: "https://birder-emails.firebaseio.com",
 };
